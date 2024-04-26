855c54009af443aeacb81ea144440e1ed36183c1
NiKiT0S1 <bobilevnikita88@gmail.com>
2024-04-27 00:06:26 +0500

    Update Main class and add comment

--- src/Main.java
+++ src/Main.java
@@ -38,6 +38,24 @@ public class Main {
             System.out.println(fruit);
         }
 
+        // Add an element at the last position
+        myList.addLast("Elderberry");
+        System.out.println("\nAfter adding 'Elderberry' at the last position:");
+
+        // Print all elements using iterator
+        for (String fruit : myList) {
+            System.out.println(fruit);
+        }
+
+        // Remove the first element
+        myList.removeFirst();
+        System.out.println("\nAfter removing the first element:");
+
+        // Print all elements using iterator
+        for (String fruit : myList) {
+            System.out.println(fruit);
+        }
+
         // Remove the last element
         myList.removeLast();
         System.out.println("\nAfter removing the last element:");
@@ -47,7 +65,38 @@ public class Main {
             System.out.println(fruit);
         }
 
-        // Clear the list
+        // Set new element by index
+        myList.set(1, "Banana");
+        System.out.println("\nAfter set at index 1:");
+
+        // Print all elements using iterator
+        for (String fruit : myList) {
+            System.out.println(fruit);
+        }
+
+//        myList.sort();
+
+//        myList.size();
+
+//        System.out.println("\nAfter finding 'Apple' by index: " + myList.indexOf("Apple"));
+
+//        System.out.println("\nAfter finding 'Apple' by index: " + myList.lastIndexOf("Apple"));
+
+        // Check that element exists in list
+        System.out.println("\nAfter check that the element 'Date' exist: " + myList.exists("Date"));
+
+        // Check size of list
+        System.out.println("\nAfter check the size of the list: " + myList.size());
+
+        // Change from list to array and output array
+        Object[] array = myList.toArray();
+        System.out.println("\nAfter change from list to array: " + array.length);
+        System.out.println("\nArray of elements:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+        // Clear the list and output size after removing
         myList.clear();
         System.out.println("\nAfter clearing the list, size: " + myList.size());
     }
